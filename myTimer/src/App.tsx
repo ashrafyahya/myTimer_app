7b8e8c927a19716494c7b6efd591d724a1fd09e5
ashrafyahya <ashrafsadiq97@gmail.com>
2024-06-13 18:46:12 +0200

    zwischen version

--- myTimer/src/App.tsx
+++ myTimer/src/App.tsx
@@ -36,7 +36,7 @@ const App: React.FC = () => (
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
-    <IonContent>
+    <IonContent color="danger">
       <MyTimer />
     </IonContent>
   </IonApp>
--- myTimer/src/pages/Menu/SettingModal.tsx
+++ myTimer/src/pages/Menu/SettingModal.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
-import { IonButtons, IonButton, IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonIcon } from '@ionic/react';
+import { IonButton, IonButtons, IonCheckbox, IonContent, IonHeader, IonIcon, IonItem, IonList, IonModal, IonPage, IonRange, IonSelect, IonSelectOption, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { settingsOutline } from 'ionicons/icons';
+import { useState } from 'react';
 
 export function SettingModal() {
     const [isOpen, setIsOpen] = useState(false);
 
+    const [checked, setChecked] = useState(false);
+
+    const handleCheckboxChange = (event: { detail: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
+        setChecked(event.detail.checked);
+        console.log('Checkbox value:', event.detail.checked);
+    };
     return (
         <IonPage>
             <IonHeader>
@@ -28,9 +34,40 @@ export function SettingModal() {
                         </IonToolbar>
                     </IonHeader>
                     <IonContent className="ion-padding">
-                        <p>
-                            Here is our setting for sound, vibration, background color etc. as share button too
-                        </p>
+                        {/* TODO
+                        use the valuse below to specify the color, sound and sound strenght, vibration ...
+                        use for that a varibale or functions, that allow you sending this values to the tabs
+                        suggestion: find out how to set the color in myTimer.tsx, so it would be easier
+                        */}
+                        <IonList>
+                            <IonItem>
+                                <IonSelect label="Color" placeholder="Favorite color">
+                                    <IonSelectOption value="red">Red</IonSelectOption>
+                                    <IonSelectOption value="black">Black</IonSelectOption>
+                                    <IonSelectOption value="white">White</IonSelectOption>
+                                </IonSelect>
+                            </IonItem>
+                            <IonItem>
+                                <IonSelect label="Sound" labelPlacement="fixed" placeholder="Favorite sound">
+                                    <IonSelectOption value="sound1">Sound1</IonSelectOption>
+                                    <IonSelectOption value="sound2">Sound2</IonSelectOption>
+                                    <IonSelectOption value="sound3">Sound3</IonSelectOption>
+                                </IonSelect>
+                            </IonItem>
+                        </IonList>
+
+                        <IonRange>
+                            <div slot="label">
+                                <IonText color="primary">Sound strength</IonText>
+                            </div>
+                        </IonRange>
+                        <IonItem>
+                            <IonCheckbox id='checkBox'
+                                checked={checked}
+                                onIonChange={handleCheckboxChange}
+                                style={{ marginLeft: 'auto' }}>Vibration</IonCheckbox>
+                        </IonItem>
+
                     </IonContent>
                 </IonModal>
             </IonContent>
